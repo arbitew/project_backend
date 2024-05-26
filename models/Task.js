d8c12ce66558a41559444fc637e6c964f75c798c
arbitew <121195731+arbitew@users.noreply.github.com>
2024-05-26 21:49:12 +0300

    Add files via upload

---
+++ models/Task.js
@@ -0,0 +1,19 @@
+const {Schema, model} = require('mongoose')
+const mongoose = require('mongoose') // for mongoDB
+
+const schema = new Schema({ //Task schema
+    groupId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Group',
+        required:true,
+    },
+    task:{
+        type: String,
+        required: true,
+    },
+    completed:{
+        type: Boolean,
+        required: true,
+    }
+})
+module.exports = model('Task', schema)
