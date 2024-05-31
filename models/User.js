89830266889736024dff43e3e0fd649e704d3a3d
arbitew <121195731+arbitew@users.noreply.github.com>
2024-05-31 12:18:43 +0300

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const {Schema, model} = require('mongoose')
+
+const schema = new Schema({ //creating todo model with columns whick described below
+    login: {//title(what to do) 
+        type: String,
+        unique: true,
+        require:true
+    },
+    name: {//
+        type: String,
+        unique: true,
+        require:true
+    },
+    password: {//
+        type: String,
+        require:true
+    },
+    ava: {//choose your ava
+        type: BigInt,
+        require:true
+    },
+    satiety:{
+        type: BigInt,
+        require: true
+    },
+    lastTODO:{
+        type: Date,
+        require: true
+    }
+})
+module.exports = model('User', schema)
